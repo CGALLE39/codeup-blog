d04970c0abd24a8689162976e2e428ee9288a8d8
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2022-08-02 11:59:32 -0500

    math table

---
+++ src/main/java/com/codeup/springblog/controllers/MathController.java
@@ -0,0 +1,33 @@
+package com.codeup.springblog.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class MathController {
+
+    @RequestMapping(path = "/add/{num}/and/{num2}", method = RequestMethod.GET)
+    @ResponseBody
+    public String addEm(@PathVariable int num, @PathVariable  int num2) {
+        return  "The sum is " + (num + num2) + "!";
+    }
+
+    @RequestMapping(path = "/subtract/{num}/and/{num2}", method = RequestMethod.GET)
+    @ResponseBody
+    public String subtractEm(@PathVariable int num, @PathVariable int num2) {
+        return "The difference is " + (num - num2) + "!";
+    }
+
+    @RequestMapping(path = "/multiply/{num}/and/{num2}", method = RequestMethod.GET)
+    @ResponseBody
+    public String multiplyEm(@PathVariable int num, @PathVariable int num2) {
+        return "The product is " + (num * num2) + "!";
+    }
+
+    @RequestMapping(path = "/divide/{num}/from/{num2}", method = RequestMethod.GET)
+    @ResponseBody
+    public String divideEm(@PathVariable int num, int num2) {
+        return "The remainder is " + (num / num2) + "!";
+    }
+}
