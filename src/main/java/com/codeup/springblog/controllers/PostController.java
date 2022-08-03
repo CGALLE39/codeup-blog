1e3910c09069d7971621f76704616f99e3a3aaab
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2022-08-03 14:32:33 -0500

    finished

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,28 +1,36 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.model.Post;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class PostController {
     @GetMapping("/posts")
-    @ResponseBody
-    public String posts() {
+   public String all(Model model) {
+        List<Post> posts = new ArrayList<>();
+        posts.add(new Post(1, "post1", "post1"));
+        posts.add(new Post(2, "post2", "post2"));
+        posts.add(new Post(3, "post3", "post3"));
+        model.addAttribute("posts", posts);
         return "/posts/index";
     }
 
-    @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
-    @ResponseBody
-    public String viewAd(@PathVariable long id) {
-        return "/posts/view";
+    @GetMapping("/posts/{id}")
+    public String postByIdPage(@PathVariable long id, Model model){
+        List<Post> posts = new ArrayList<>();
+        posts.add(new Post(1L, "Post 1", "This is my post 1"));
+        posts.add(new Post(2L, "Post 2", "This is my post 2"));
+        posts.add(new Post(3L, "Post 3", "This is my post 3"));
+
+        model.addAttribute("post", posts.get((int)id - 1));
+
+        return "/posts/show";
     }
 
-    @RequestMapping (value = "/posts/create", method = RequestMethod.GET)
-    @ResponseBody
-    public String viewCreate() {
-        return "/posts/create";
-    }
-    @PostMapping (path = "/posts/create")
-    public String postCreate() {
-        return null;
-    }
 }
 
--- src/main/java/com/codeup/springblog/model/Post.java
+++ src/main/java/com/codeup/springblog/model/Post.java
@@ -1,18 +1,31 @@
-package com.codeup.springblog.controllers;
+package com.codeup.springblog.model;
 
 
-import org.springframework.stereotype.Controller;
 
 public class Post {
 
     private String title;
 
+    private long id;
+
     private String body;
 
-    public Post(String title) {
+    public Post (long id, String title, String body) {
+        this.body = body;
+        this.id = id;
         this.title = title;
     }
 
+
+public Post() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
     public String getBody() {
         return body;
     }
